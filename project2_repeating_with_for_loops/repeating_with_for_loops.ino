53d267877f6ac07a742ccf21e2e17463f93f895f
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2019-05-17 18:52:55 +0500

    project2_repeating_with_for_loops

---
+++ project2_repeating_with_for_loops/repeating_with_for_loops.ino
@@ -0,0 +1,20 @@
+int d = 100;
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+  for ( int a = 2; a < 7 ; a++ )
+  {
+    digitalWrite(a, HIGH);
+    delay(d);
+    digitalWrite(a, LOW);
+    delay(d);
+  }
+}
