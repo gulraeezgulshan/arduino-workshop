994fb8de41ba8fde1cc03874e0c52ffbc92193ae
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2019-05-18 21:18:03 +0500

    project37

---
+++ project37_creating_own_breadboard_arduino/creating_own_breadboard_arduino/creating_own_breadboard_arduino.ino
@@ -0,0 +1,12 @@
+// Project 37 - Creating Your Own Breadboard Arduino
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
